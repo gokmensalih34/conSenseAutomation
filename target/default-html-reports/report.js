7b1615916e8ad2502c217efe9d1ce0975714ab97
gokmensalih34 <gokmensalih34@gmail.comgit config --listgit config --listgit config --global user.password 34253812SGsg>
2020-10-29 23:22:30 +0100

    modify

--- target/default-html-reports/report.js
+++ target/default-html-reports/report.js
@@ -1,63 +1,4 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/aaa-zahlen-daten-fakten.feature");
-formatter.feature({
-  "name": "Verifyig the foundation year",
-  "description": "",
-  "keyword": "Feature",
-  "tags": [
-    {
-      "name": "@wip"
-    }
-  ]
-});
-formatter.scenario({
-  "name": "The foundation year should see be same",
-  "description": "",
-  "keyword": "Scenario",
-  "tags": [
-    {
-      "name": "@wip"
-    }
-  ]
-});
-formatter.before({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user access HomePage",
-  "keyword": "Given "
-});
-formatter.match({
-  "location": "com.conSense.stepdefinitions.HomePageStepDefinitions.the_user_access_HomePage()"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the user navigate to \"zahlen-daten-fakten\"",
-  "keyword": "When "
-});
-formatter.match({
-  "location": "com.conSense.stepdefinitions.ZahlenDatenFaktenStepdefinitions.the_user_navigate_to(java.lang.String)"
-});
-formatter.result({
-  "status": "passed"
-});
-formatter.step({
-  "name": "the foundation years are same at \"HomePage\" and \"zahlen-daten-fakten Page\"",
-  "keyword": "Then "
-});
-formatter.match({
-  "location": "com.conSense.stepdefinitions.ZahlenDatenFaktenStepdefinitions.the_foundation_years_are_same_at_and(java.lang.String,java.lang.String)"
-});
-formatter.result({
-  "error_message": "org.junit.ComparisonFailure: Verifing the displayed experice years are same expected:\u003c1[7]\u003e but was:\u003c1[5]\u003e\n\tat org.junit.Assert.assertEquals(Assert.java:117)\n\tat com.conSense.stepdefinitions.ZahlenDatenFaktenStepdefinitions.the_foundation_years_are_same_at_and(ZahlenDatenFaktenStepdefinitions.java:64)\n\tat ✽.the foundation years are same at \"HomePage\" and \"zahlen-daten-fakten Page\"(file:///Users/sgok/IdeaProjects/conSenseAutomation/src/test/resources/features/aaa-zahlen-daten-fakten.feature:6)\n",
-  "status": "failed"
-});
-formatter.embedding("image/png", "embedded0.png", "screenshot");
-formatter.after({
-  "status": "passed"
-});
-formatter.uri("file:src/test/resources/features/homePage.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/features/homePage.feature");
 formatter.feature({
   "name": "Verifying Tabs",
   "description": "",
@@ -134,7 +75,7 @@ formatter.after({
 });
 formatter.uri("file:src/test/resources/features/zahlen-daten-fakten.feature");
 formatter.feature({
-  "name": "Verifyig the foundation year",
+  "name": "Verifying the foundation year",
   "description": "",
   "keyword": "Feature",
   "tags": [
@@ -144,7 +85,7 @@ formatter.feature({
   ]
 });
 formatter.scenario({
-  "name": "The foundation year should see be same",
+  "name": "The user should see the foundation year same at Home page and other pages",
   "description": "",
   "keyword": "Scenario",
   "tags": [
@@ -187,7 +128,7 @@ formatter.result({
   "error_message": "org.junit.ComparisonFailure: Verifing the displayed experice years are same expected:\u003c1[7]\u003e but was:\u003c1[5]\u003e\n\tat org.junit.Assert.assertEquals(Assert.java:117)\n\tat com.conSense.stepdefinitions.ZahlenDatenFaktenStepdefinitions.the_foundation_years_are_same_at_and(ZahlenDatenFaktenStepdefinitions.java:64)\n\tat ✽.the foundation years are same at \"HomePage\" and \"zahlen-daten-fakten Page\"(file:///Users/sgok/IdeaProjects/conSenseAutomation/src/test/resources/features/zahlen-daten-fakten.feature:6)\n",
   "status": "failed"
 });
-formatter.embedding("image/png", "embedded1.png", "screenshot");
+formatter.embedding("image/png", "embedded0.png", "screenshot");
 formatter.after({
   "status": "passed"
 });
